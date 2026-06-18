85c0f573befcce2f9ce846899523de11135ecba8
USER <samberhanuclassno67@gmail.com>
2026-06-18 14:39:38 +0300

    Fix: Add H2 test database config to make AuthControllerTest and FleetflowApplicationTests pass without Docker

--- src/test/java/com/fleetflow/AuthControllerTest.java
+++ src/test/java/com/fleetflow/AuthControllerTest.java
@@ -9,6 +9,7 @@ import com.fleetflow.service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthControllerTest {
 
     @Autowired
